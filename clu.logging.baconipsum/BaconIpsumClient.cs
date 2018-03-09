93001194e80ced198bf2f22bc38af59af5bdb506
Camiel Luijkx <camiel.luijkx@gmail.com>
2018-03-09 21:31:52 +0100

    missing files

---
+++ clu.logging.baconipsum/BaconIpsumClient.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace clu.logging.baconipsum
+{
+    public static class BaconIpsumClient
+    {
+        public static async Task<string> GetAsync()
+        {
+            var data = "";
+
+            var client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync("https://baconipsum.com/api/?type=all-meat&paras=1&start-with-lorem=0&format=text");
+
+            if (response.IsSuccessStatusCode)
+            {
+                data = await response.Content.ReadAsStringAsync();
+            }
+
+            return data;
+        }
+    }
+}
